94c9a1af3005acae3b22249802d39f98f951286d
Oscar Daniel Perez Espinoza <odprz.dev@gmail.com>
2020-03-29 14:29:11 -0600

    feat: implementacion de identityserver para proteger una api con credenciales de cliente

---
+++ src/Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        private static async Task Main()
+        {
+
+            // discover endpoints from metadata
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secreto",
+
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine("Error al generar el tokén: "+tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine("Token generado: "+"\n"+ tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("http://localhost:5001/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Se generó un error en la petición:"+response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+        }
+
+
+    }
+}
+
+   
---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            {
+                new ApiResource("api1"," My API")
+            };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { 
+                new Client
+                {
+                    ClientId = "client",
+                    // usuario no interactivo, usa el clientid/secret para autenticarse
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secreto".Sha256())
+                    },
+
+                    // scopes que el cliente tiene acceso
+                    AllowedScopes = {"api1"}
+                }
+            };
+        
+    }
+}
